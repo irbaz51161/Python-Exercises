bee26653b6dedcf0ee5df5a013ac64fb649305bd
MUHAMMAD IRBAZ KARAMAT <rehankaramat12@gmail.com>
2024-08-23 22:34:24 +0500

    2.3

---
+++ Adeel Python Exercises/2.3 List intersection.py
@@ -0,0 +1,21 @@
+# def comparing(l1,l2):
+
+
+
+l = int(input("How many numbers you want to enter into the list? : "))
+list1 = []
+for _ in range(1, l+1):
+    num = int(input(f"Enter number {_}: "))
+    list1.append(num)
+m = int(input("How many numbers you want to enter into the list? : "))
+list2 = []
+for _ in range(1, m+1):
+    num0 = int(input(f"Enter number {_}: "))
+    list2.append(num0)
+
+print("List#01", list1)
+print("List#02", list2)
+
+
+
+comparing(list1, list2)
