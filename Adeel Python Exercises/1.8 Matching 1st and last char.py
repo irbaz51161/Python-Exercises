a6f07772356a6b696a8051b34cbb42c3c3601b67
MUHAMMAD IRBAZ KARAMAT <rehankaramat12@gmail.com>
2024-08-23 08:23:50 +0500

    completed 1.8

--- Adeel Python Exercises/1.8 Matching 1st and last char.py
+++ Adeel Python Exercises/1.8 Matching 1st and last char.py
@@ -5,7 +5,11 @@ def taking_user_list(abc, mno):
     print(f"Your created list is: {mno}")
 
 def matching(mno):
-    print()
+    count=0
+    for words in for_list:
+        if len(words) >= 2 and words[0] == words[-1]:
+            count+=1
+    print(f"Number of matching words: {count}")
 
 word = int(input("How many words you want to enter into the list: "))
 for_list = []
